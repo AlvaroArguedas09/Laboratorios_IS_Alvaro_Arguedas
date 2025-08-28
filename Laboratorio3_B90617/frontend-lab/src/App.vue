05edab9ed61118e248ab182e5266e97ea45608a2
AlvaroArguedas09 <alvaro.arguedasvillegas@ucr.ac.cr>
2025-08-28 16:12:53 -0600

    Se crea el primer componente CountriesList

--- Laboratorio3_B90617/frontend-lab/src/App.vue
+++ Laboratorio3_B90617/frontend-lab/src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Alvaro Arguedas"/>
+    <CountriesList />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    import CountriesList from "./components/CountriesList.vue";
+    export default {
+        name: 'App',
+        components: {
+            CountriesList,
+        },
+    };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ Laboratorio3_B90617/frontend-lab/src/components/CountriesList.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container mt-5">
+      <h1 class="display-4 text-center">Lista de paises</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+      name: "CountriesList"     
+  }
+</script>
+
+<style lang="scss" scope> </style>
