f74d49a946b224d18a4668f788f70a0c140443ad
AlvaroArguedas09 <alvaro.arguedasvillegas@ucr.ac.cr>
2025-09-04 07:25:22 -0600

    Laboratorio # 4

---
+++ backend-lab-b90617/backend-lab-b90617/Controllers/CountryController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend_lab_b90617.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hola Mundo";
+
+        }
+    }
+
+}
